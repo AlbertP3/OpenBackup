86d682e4a3e2394a186816de37eac802fbce9ec5
AlbertP3 <psyduckdebugging@gmail.com>
2023-06-21 20:38:50 +0200

    debugging, improved inheritance, refactor

--- base.py
+++ base.py
@@ -6,6 +6,7 @@ import os
 
 class BasicGenerator(ABC):
     re_space = re.compile(r'(?<!\\) ')
+    SWD = os.path.dirname(os.path.abspath(__file__))
 
     @abstractmethod
     def generate(self) -> list:
@@ -13,16 +14,22 @@ class BasicGenerator(ABC):
 
     def get_target_path(self, path:dict) -> str:
         '''Returns path where the file will be stored on destination'''
-        return os.path.normpath(os.path.join(path.get('dst', '.'), path['src']))
-    
-    def get_start_path(self, path:dict) -> str:
-        '''Returns relative path on destination where the file/dir should exist'''
-        try:
-            return path['dst']
-        except KeyError:
-            return os.path.basename(path['src'])
+        return os.path.normpath(os.path.join(path['dst'], path['src']))
 
     def parse_path(self, path:str) -> str:
-        '''Escape spaces in paths'''
+        '''Parse single path'''
         path = self.re_space.sub('\ ', path)
         return path
+
+    def parse_config(self, config:dict) -> dict:
+        self.SWD = self.parse_path(self.SWD)
+        config['rsync']['settings']['rlogfilename'] = self.parse_path(config['rsync']['settings']['rlogfilename'])
+        for i, v in enumerate(config['rsync']['settings']['mkdirs']):
+            config['rsync']['settings']['mkdirs'][i] = self.parse_path(v)
+        for i, v in enumerate(config['rsync']['paths']):
+            config['rsync']['paths'][i]['src'] = self.parse_path(v['src'])
+            try:
+                config['rsync']['paths'][i]['dst'] = self.parse_path(v['dst'])
+            except KeyError:
+                config['rsync']['paths'][i]['dst'] = '.'
+        return config
--- prepare_script.py
+++ prepare_script.py
@@ -9,33 +9,19 @@ class RsyncGenerator(BasicGenerator):
        to upload missing/modified files and a TreeMonitor to track renamed/moved/deleted'''
 
     def __init__(self, config):
-        self.SWD = os.path.dirname(os.path.abspath(__file__))
-        self.__config = config
-        self.config:dict = self.__config['rsync']
-        self.settings:dict = self.config['settings']
-        self.parse_paths()
-        self.logpath = f"{self.settings['rlogfilename']}"
+        self.config = config
+        self.logpath = self.config['rsync']['settings']['rlogfilename']
     
-    def parse_paths(self):
-        '''Parse all available paths - escape spaces'''
-        self.settings['rlogfilename'] = self.parse_path(self.settings['rlogfilename'])
-        self.SWD = self.parse_path(self.SWD)
-        for i in range(len(self.config['paths'])):
-            self.config['paths'][i]['src'] = self.parse_path(self.config['paths'][i]['src'])
-            try:
-                self.config['paths'][i]['dst'] = self.parse_path(self.config['paths'][i]['dst'])
-            except KeyError:
-                pass
-
     def generate(self) -> list:
         '''Create a list of all operations - foundament of the bash script'''
         self.out:list = list()
         self.gen_header()
         self.gen_logging()
         self.gen_mkdirs()
+        self.gen_cmds('pre')
         self.gen_monitor_actions()
         self.gen_rsync()
-        self.gen_post_cmds()
+        self.gen_cmds('post')
         return self.out
     
     def gen_header(self):
@@ -46,30 +32,29 @@ class RsyncGenerator(BasicGenerator):
         self.out.append('# Setup logging')
         self.out.extend([
             f'echo -n > {self.logpath}',
-            f'''log="--log-file={self.logpath} --log-file-format={self.settings['rlogfileformat']}"''',
+            f'''log="--log-file={self.logpath} --log-file-format={self.config['rsync']['settings']['rlogfileformat']}"''',
             ''
         ])
 
     def gen_rsync(self):
         self.out.append("# Sync files")
-        for v in self.config['paths']:
+        for v in self.config['rsync']['paths']:
             if v.get('archive') and not v.get('require_closed'):
                 self.out.append(self.get_archive_cmd(v))
                 continue
-            src = v['src']
-            tgt = v.get('dst', '.')
-            mode = self.settings['rconfmode'] if v.get('isconf') else self.settings['rmode']
+            mode = self.config['rsync']['settings']['rconfmode'] if v.get('isconf') else self.config['rsync']['settings']['rmode']
             exclusions = "--exclude={"+','.join(v['exclude'])+"}" if v.get('exclude') else ''
-            c = f"""rsync -{mode} {src} {tgt} $log {exclusions}"""
+            c = f"""rsync -{mode} {v['src']} {v['dst']} $log {exclusions}"""
             if v.get('require_closed'):
                 self.gen_require_closed(c, v)
             else:
                 self.out.append(c)
 
-    def gen_post_cmds(self):
-        if not self.settings.get('cmd', {}).get('post'): return
-        self.out.extend(['', '# Post Commands'])
-        for c in self.settings['cmd']['post']:
+    def gen_cmds(self, which:str):
+        '''Generate which:(pre,post) commands if available'''
+        if not self.config['rsync']['settings'].get('cmd', {}).get(which): return
+        self.out.extend(['', f'# {which.capitalize()} Commands'])
+        for c in self.config['rsync']['settings']['cmd'][which]:
             self.out.append(self.parse_cmd(c))
 
     def gen_require_closed(self, cmd:str, path):
@@ -83,7 +68,7 @@ class RsyncGenerator(BasicGenerator):
 
     def gen_mkdirs(self):
         self.out.append('# Create directories')
-        for d in self.config['settings'].get('mkdirs', []):
+        for d in self.config['rsync']['settings'].get('mkdirs', []):
             self.out.append(f"mkdir -p {d}")
         self.out.append('')
         
@@ -93,10 +78,9 @@ class RsyncGenerator(BasicGenerator):
         return cmd
 
     def gen_monitor_actions(self):
-        monitor = TreeMonitor(self.__config)
+        monitor = TreeMonitor(self.config)
         self.out.extend(["# Apply changes (renamed/deleted/moved)", *monitor.generate(), ''])
         
     def get_archive_cmd(self, path) -> str:
-        dst = path['src']
-        src = os.path.dirname(self.get_target_path(path))
-        return f"tar -cjf {src}/{path['archive']}.bz2 {dst}"
+        tgt = self.get_target_path({'dst': path['dst'], 'src': path['archive']})
+        return f"tar -cjf {tgt}.bz2 {path['src']}"
