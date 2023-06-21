b33be4ca123244e82bba0eb79905ab3e214f823b
AlbertP3 <psyduckdebugging@gmail.com>
2023-06-21 20:47:35 +0200

    more tests

--- tests/scenarios.py
+++ tests/scenarios.py
@@ -1,3 +1,5 @@
+from . import SWD
+
 
 SCENARIO_SRC = [
         {'dir1' : 
@@ -6,7 +8,7 @@ SCENARIO_SRC = [
                 'c.csv', 'd.cpp', {
                 'venv': [
                     'e.whl', 'f.h']}],
-            'dir4': {'h.html', 'i.ini'},
+            'dir 4': {'h.html', 'i.ini'},
             'dir5': []
             }, '__k.pdf']
         },
@@ -21,7 +23,7 @@ SCENARIO_TGT = [
                 'c.csv', 'd.cpp', {
                 'venv': [
                     'e.whl', 'f.h', 'r_n.exe']}],
-            'dir4': ['h.html', 'r_i.ini'],
+            'dir 4': ['h.html', 'r_i.ini'],
             'r_dir5': ['r_j.jpg', 'r_k.rtf']
             }
             ]
@@ -29,3 +31,59 @@ SCENARIO_TGT = [
         'r_g.xml',
         'g.xml'
     ]
+
+
+EXP_GEN_RSYNC = [
+    '# Sync files',
+    'if pgrep some_pid; then',
+    '  echo "ERROR some_pid must be closed in order to backup the configuration" >> some/pa\\ th/test.log',
+    'else',
+    f'  tar -cjf arch.bz2 {SWD}/data/src/dir1',
+    'fi',
+    f'rsync -truOvn {SWD}/data/src/g.xml . $log ',
+    f'rsync -truOvn {SWD}/data/src/'+'{h.go,l.doc} . $log '
+]
+
+EXP_PREPARE_SCRIPT_ACTIONS = [
+'# Apply changes (renamed/deleted/moved)',
+'rm -rfv dir1/r_dir5 | tee -a some/pa\\ th/test.log',
+'rm -rfv dir1/r_\\ b.txt | tee -a some/pa\\ th/test.log',
+'rm -rfv dir1/dir\\ 4/r_i.ini | tee -a some/pa\\ th/test.log',
+''
+]
+
+EXP_GENERATE_PREPARE_SCRIPT = [
+    '#!/usr/bin/env bash',
+    '',
+    '# Enable Pathname Expansion',
+    'shopt -s extglob',
+    '',
+    '# Setup logging',
+    'echo -n > some/pa\\ th/test.log',
+    'log="--log-file=some/pa\\ th/test.log --log-file-format=/%f"',
+    '',
+    '# Create directories',
+    'mkdir -p dir1/new_dir',
+    '',
+    '# Pre Commands',
+    "echo 'Goodbye' | tee some/pa\\ th/test.log",
+    'man tee',
+    '# Apply changes (renamed/deleted/moved)',
+    'rm -rfv dir1/dir\\ 4/r_i.ini | tee -a some/pa\\ th/test.log',
+    'rm -rfv dir1/r_dir5 | tee -a some/pa\\ th/test.log',
+    'rm -rfv dir1/r_\\ b.txt | tee -a some/pa\\ th/test.log',
+    '',
+    '# Sync files',
+    'if pgrep some_pid; then',
+    '  echo "ERROR some_pid must be closed in order to backup the configuration" >> some/pa\\ th/test.log',
+    'else',
+    '  tar -cjf arch.bz2 '
+    f'{SWD}/data/src/dir1',
+    'fi',
+    f'rsync -truOvn {SWD}/data/src/g.xml . $log ',
+    'rsync -truOvn '
+    f'{SWD}/data/src/' + '{h.go,l.doc} . $log ',
+    '',
+    '# Post Commands',
+    "echo 'Hello, world'"
+]
